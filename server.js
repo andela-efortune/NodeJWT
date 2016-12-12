1d6cc119ef9ff6c5fdadfa70ec0ebe86f2be42cd
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-12-12 17:55:55 +0100

    Add authenticate route that creates and returns a token if the user's name and password is found in the db and it matches

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/setup', (req, res) => {
 
   // create a sample user
   var nick = new User({
-    name: 'Fortune Ekeruo',
+    name: 'Nick Cerminara',
     password: 'password',
     admin: true
   });
@@ -58,6 +58,43 @@ app.get('/setup', (req, res) => {
 const apiRoutes = express.Router();
 
 // TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', (req, res)  => {
+
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, (err, user) => {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        const token = jwt.sign(user, app.get('superSecret'), {
+          expiresIn: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+
+    }
+
+  });
+});
 
 // TODO: route middleware to verify a token
 
