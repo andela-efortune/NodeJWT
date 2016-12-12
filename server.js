8501d2f648d7ef358f4ab59277e53f25b57e68ae
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-12-12 18:41:18 +0100

    Use arrow functions and const from es6

--- server.js
+++ server.js
@@ -98,16 +98,16 @@ apiRoutes.post('/authenticate', (req, res)  => {
 
 // TODO: route middleware to verify a token
 // route middleware to verify a token
-apiRoutes.use(function(req, res, next) {
+apiRoutes.use((req, res, next) => {
 
   // check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  const token = req.body.token || req.query.token || req.headers['x-access-token'];
 
   // decode token
   if (token) {
 
     // verifies secret and checks exp
-    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+    jwt.verify(token, app.get('superSecret'), (err, decoded) => {
       if (err) {
         return res.json({ success: false, message: 'Failed to authenticate token.' });
       } else {
@@ -128,7 +128,6 @@ apiRoutes.use(function(req, res, next) {
 
   }
 });
-
 
 // route to show a random message (GET http://localhost:8080/api/)
 apiRoutes.get('/', (req, res) => {
